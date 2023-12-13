86f7732d46b2519277c36d3e233f7fb5aa3d6af7
ShugoYoko <yokotnaname@gmail.com>
2023-12-13 17:36:06 +0900

    プロジェクト ファイルを追加します。

---
+++ EnglishCompare.Con/Domain/WordSet.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace EnglishCompare.Con.Domain;
+
+public class WordSet
+{
+    HashSet<Word> _words;
+
+    public WordSet()
+    {
+        this._words = new HashSet<Word>();
+    }
+
+
+    public int GetCount()
+    {
+        return this._words.Count;
+    }
+
+    public void Add(string content)
+    {
+        var words = content.Split(',', ':', '!', '?',' ','.');
+        foreach (var word in words)
+        {
+            var addWord = new Word(word);
+            if (addWord != null && addWord.CanAdd() && word!="")
+            {
+                this._words.Add(addWord);
+            }
+        }
+    }
+
+    public HashSet<Word> GetAll()
+    {
+        HashSet<Word> result = new HashSet<Word>();
+        foreach(var w in this._words)
+        {
+            result.Add(w);
+        }
+        return result;
+    }
+
+    public HashSet<Word> ReturnMatch(HashSet<Word> wordSet)
+    {
+        if (wordSet != null)
+        {
+            var sets = this.GetAll();
+            sets.IntersectWith(wordSet);
+            return sets;
+        }
+
+        var empty = new HashSet<Word>();
+        return empty;
+
+    }
+
+    public HashSet<Word> ReturnNonMatch(HashSet<Word> wordSet)
+    {
+        if (wordSet != null)
+        {
+            var sets = this.GetAll();
+            sets.ExceptWith(wordSet);
+            return sets;
+        }
+
+        return this._words;
+
+    }
+
+
+}
---
+++ EnglishCompare.Con/Program.cs
@@ -0,0 +1,23 @@
+﻿using EnglishCompare.Con.Domain;
+using EnglishCompare.Con.UseCase;
+using EnglishCompare.Con.Infrastructure;
+
+
+namespace EnglishCompare.Con;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string targetPath = @"./Target";
+        string dataPath = @"./Database";
+        IWordSetRepo target = new WordSetFileRepo(targetPath);
+        IWordSetRepo database=new WordSetFileRepo(dataPath);
+
+        var diplayCase = new DisplayResult(target, database);
+        diplayCase.Run();
+        Console.ReadLine();
+
+
+    }
+}
